9776b181dfc97dfb9c5a640cb15e6a9def703670
Maxim Danyuk <MaximDanyuk@yandex.ru>
2022-07-09 17:04:13 +0300

    feat: добавил защиту от переполнения, закрытие попапа по эскейпу и добавил ссылку на проект в ридми

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ closeButton.addEventListener("click", function () {
   popup.classList.remove("popup_opened");
 });
 
-/// Повысить качество пользовательского опыта, закрытие попапа по эскейпу, давно привычная фича
-/* document.addEventListener("keydown", function (evt) {
+/// Повысить качество пользовательского опыта, закрытие попапа по эскейпу, давно привычная фича, можно оставить?
+document.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     popup.classList.remove("popup_opened");
   }
 });
- */
+
 /* Переменные инпутов попапа, формы и кнопки */
 let firstName = document.querySelector(".popup__input-name");
 let lastName = document.querySelector(".popup__input-about");
-/// let saveButton = document.querySelector(".popup__submit-btn_action_add");
+let saveButton = document.querySelector(".popup__submit-btn_action_add");
 
 let form = document.querySelector(".popup__form");
 
@@ -35,26 +35,26 @@ function formSubmitHandler(evt) {
   popup.classList.remove("popup_opened");
 
   ///   Защита от переполнения имени
-  /*   if (firstName.value.length > 20) {
+  if (firstName.value.length > 20) {
     profileName.style.fontSize = "2.6vw";
   } else {
     profileName.style.fontSize = "3.28vw";
   }
- */
+
   ///   Защита от переполнения описания, вопрос не по спринту, почему код после элсе не работает?
 
-  /*   if (lastName.value.length > 60) {
+  if (lastName.value.length > 60) {
     saveButton.setAttribute("disabled", true);
   } else {
     saveButton.removeAttribute("disabled");
-  } */
+  }
 }
 
 form.addEventListener("submit", formSubmitHandler);
 
 /* Вопросы не по спринту, заметил, что не работают никакие кнопки по нажатию на энтер, надо добавить обработчик событий по нажатию на эту кнопку?  */
 
-/*  Подскажите на будущее, что не так? 
+/*  Пытался лайки сделать....что-то не так
 /// Переменные для лайков
 
 let hearts = document.querySelectorAll(".card__heart");
