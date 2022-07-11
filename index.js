c297d6f502ba5f206b585feddc02cc1d5db77bcf
Maxim Danyuk <MaximDanyuk@yandex.ru>
2022-07-11 09:49:18 +0300

    refactoring: исменить классы в футере

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ closeButton.addEventListener("click", function () {
   popup.classList.remove("popup_opened");
 });
 
-/// Повысить качество пользовательского опыта, закрытие попапа по эскейпу, давно привычная фича, можно оставить?
-document.addEventListener("keydown", function (evt) {
+document.addEventListener("keydown", function () {
   if (evt.keyCode === 27) {
+    popup.classList.remove("popup_opened");
+  }
+});
+
+popup.addEventListener("click", function () {
+  if (event.target === event.currentTarget) {
     popup.classList.remove("popup_opened");
   }
 });
@@ -54,21 +59,21 @@ form.addEventListener("submit", formSubmitHandler);
 
 /* Вопросы не по спринту, заметил, что не работают никакие кнопки по нажатию на энтер, надо добавить обработчик событий по нажатию на эту кнопку?  */
 
-/// Переменные для лайков
+/// Лайки на будущее,можете тоже, пожалуйста, проверить?
 
-let hearts = document.querySelectorAll(".card__heart");
+/* let hearts = document.querySelectorAll(".card__heart");
 
-for (heart of hearts) {
-  heart.addEventListener("click", function () {
-    heart.classList.toggle("card__heart_condition_active");
+for (let i = 0; i < hearts.length; i += 1) {
+  hearts[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    hearts[i].classList.toggle("card__heart_condition_active");
   });
 }
 
-/* heart.addEventListener("click", function () {
+ heart.addEventListener("click", function () {
   if (heart.classList.contains("card__heart_condition_active")) {
     likesNumber.textContent--; // Добавить счетчик
   } else {
     likesNumber.textContent++;
   }
-});
- */
+}); */
