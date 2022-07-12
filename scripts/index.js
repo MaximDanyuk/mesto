756f9a8617a555b70a906a42323168cd1dc18a02
Maxim Danyuk <MaximDanyuk@yandex.ru>
2022-07-12 13:51:58 +0300

    style: изменить расположение кнопки сохранить на главном экране и кнопки закрыть попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,27 @@
 /* Переменные для попапа */
 let editButton = document.querySelector('.profile__image-edit');
-let closeButton = document.querySelector('.popup__submit-btn_action_close');
+let closeButton = document.querySelector('.popup__btn-close');
 let popup = document.querySelector('.popup');
 
-editButton.addEventListener('click', function () {
+function popupOpen(evt) {
+  evt.preventDefault();
   popup.classList.add('popup_opened');
-});
+  firstName.value = profileName.textContent;
+  lastName.value = profileAbout.textContent;
+}
 
-closeButton.addEventListener('click', function () {
+editButton.addEventListener('click', popupOpen);
+
+function popupClose(evt) {
+  evt.preventDefault();
   popup.classList.remove('popup_opened');
-});
+}
+
+closeButton.addEventListener('click', popupClose);
 
 /* Переменные инпутов попапа, формы и кнопки */
-let firstName = document.querySelector('.popup__input-name');
-let lastName = document.querySelector('.popup__input-about');
-let saveButton = document.querySelector('.popup__submit-btn_action_add');
+let firstName = document.querySelector('.popup__input-name_theme_position');
+let lastName = document.querySelector('.popup__input-about_theme_position');
 
 let popupForm = document.querySelector('.popup__form');
 
